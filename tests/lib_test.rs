00ea0b90a1b8ea8806bc24ee611d58ce64074f98
danielefongo <danielefongo@gmail.com>
2021-07-02 20:03:57 +0200

    update tests

--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -1,48 +1,46 @@
 cappuccino::tests!({
-    it "should pass" {
+    it should_pass() {
         assert_eq!(1, 1);
     }
 
-    it "should pass with final void expression" {
+    it should_pass_with_final_void_expression() {
         ()
     }
 
-    it "should pass with result" -> Result<(), String> {
+    it should_pass_with_result() -> Result<(), String> {
         assert_eq!(1, 1);
         Ok(())
     }
 
-    when "empty" {}
+    when empty() {}
 
-    when "has space in description" {}
-
-    when "has single it" {
-        it "should pass" {
+    when has_single_it() {
+        it should_pass() {
             assert_eq!(1, 1);
         }
     }
 
-    when "has multiple it" {
-        it "should pass 1" {
+    when has_multiple_it() {
+        it should_pass_1() {
             assert_eq!(1, 1);
         }
-        it "should pass 2" {
+        it should_pass_2() {
             assert_eq!(1, 1);
         }
     }
 
-    when "has inner when" {
-        when "and has an it" {
-            it "should pass" {
+    when has_inner_when() {
+        when and_has_an_it() {
+            it should_pass() {
                 assert_eq!(1, 1);
             }
         }
     }
 
-    when "has hybrid when and it" {
-        when "empty" {}
+    when has_hybrid_when_and_it() {
+        when empty() {}
 
-        it "should pass" {
+        it should_pass() {
             assert_eq!(1, 1);
         }
     }
@@ -52,46 +50,46 @@ cappuccino::tests!({
         let b = 42;
     }
 
-    it "should pass using before" {
+    it should_pass_using_before() {
         assert_eq!(a, b);
     }
 
-    when "has no inner before" {
-        it "should use super before" {
+    when has_no_inner_before() {
+        it should_use_super_before() {
             assert_eq!(a, 42);
         }
     }
 
-    when "has have a nested when" {
-        when "has no inner before" {
-            it "should use super before" {
+    when has_have_a_nested_when() {
+        when has_no_inner_before() {
+            it should_use_super_before() {
                 assert_eq!(a, 42);
             }
         }
 
-        when "has inner before" {
+        when has_inner_before() {
             before {
                 let a = 24;
             }
 
-            it "should use inner before" {
+            it should_use_inner_before() {
                 assert_eq!(a, 24);
             }
         }
     }
 
-    when "has inner before" {
+    when has_inner_before() {
         before {
             let a = 24;
         }
 
-        it "should pass" {
+        it should_pass() {
             assert_eq!(a, 24);
         }
     }
 
-    when "using utility functions" {
-        it "can use define functions" {
+    when using_utility_functions() {
+        it can_use_define_functions() {
             assert_eq!(the_answer(), 42);
         }
 
@@ -102,16 +100,16 @@ cappuccino::tests!({
 });
 
 #[cfg(feature = "async")]
-cappuccino::tests!("async_tests" {
+cappuccino::tests!(async_tests() {
     before {
         let real_answer = 42;
     }
 
-    it "should pass" async {
+    it should_pass() async {
         assert_eq!(the_long_waited_answer().await, real_answer);
     }
 
-    it "should pass and return result" async -> Result<(), String> {
+    it should_pass_and_return_result() async -> Result<(), String> {
         assert_eq!(the_long_waited_answer().await, real_answer);
         Ok(())
     }
@@ -122,13 +120,13 @@ cappuccino::tests!("async_tests" {
     }
 });
 
-cappuccino::tests!(ident_tests() {
-    it should_pass() {
+cappuccino::tests!("literal ident tests" {
+    it "should pass" {
         assert_eq!(1, 1);
     }
 
-    when condition() {
-        it should_pass() {
+    when "condition" {
+        it "should pass" {
             assert_eq!(1, 1);
         }
     }
